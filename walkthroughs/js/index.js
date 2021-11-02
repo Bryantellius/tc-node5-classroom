e65f59d20ed17975dce7dd349e6d749fa506d44b
Ben Bryant <brbryant2639@gmail.com>
2021-11-02 15:22:54 -0500

    Start of DOM walkthrough

--- walkthroughs/js/index.js
+++ walkthroughs/js/index.js
@@ -1,71 +1,25 @@
-// RPG Game
+let digits = document.getElementsByClassName("digit");
+let modal = document.getElementsByClassName("modal")[0];
 
-class Player {
-  constructor() {
-    this.name;
-    this.maxHealth = 3;
-    this.overallLevel = 1;
-    this.boardPosition = 0;
-  }
+setTimeout(function () {
+  modal.style.display = "flex";
+}, 5000);
 
-  move() {
-    let moved = Math.round(Math.random() * 6);
-    this.boardPosition += moved;
-    console.log(
-      `${this.name} moved ${spaces} space(s) and is now at ${this.boardPosition}...`
-    );
-  }
-
-  editName(name) {
-    this.name = name;
-  }
+for (let span of digits) {
+  span.onclick = function () {
+    span.textContent = prompt("Change me:", span.textContent);
+  };
 }
 
-class Warrior extends Player {
-  constructor() {
-    super();
+let input = document.getElementById("message");
+let title = document.getElementById("title");
 
-    this.weapon = "sword";
-  }
+input.addEventListener("keyup", function (event) {
+  title.textContent = event.target.value;
+});
 
-  attack() {}
-}
-
-class Mage extends Player {
-  constructor() {
-    super();
-
-    this.weapon = "tome";
-  }
-
-  attack() {}
-}
-
-class Game {
-  constructor() {
-    this.players = {};
-  }
-
-  createPlayers() {
-    let playerOneName = prompt("Who is player One?");
-    let playerTwoName = prompt("Who is player Two?");
-
-    this.players.p1 = new Warrior();
-    this.players.p2 = new Mage();
-
-    this.players.p1.editName(playerOneName);
-    this.players.p2.editName(playerTwoName);
-
-    alert(
-      `Welcome ${this.players.p1.name} and ${this.players.p2.name}! Ready to code?`
-    );
-  }
-
-  startGame() {
-    alert("Welcome to Dungeon Coders!");
-
-    this.createPlayers();
-  }
-}
-
-new Game().startGame();
+document
+  .getElementsByClassName("modal-btn")[1]
+  .addEventListener("click", function () {
+    modal.style.display = "none";
+  });
