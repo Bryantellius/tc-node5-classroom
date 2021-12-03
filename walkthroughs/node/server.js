066b02b02f95edb2192995d01e9ed52fd10081c8
Ben <brbryant2639@gmail.com>
2021-12-03 10:14:52 -0600

    Web server example with imported routes

--- walkthroughs/node/server.js
+++ walkthroughs/node/server.js
@@ -1,85 +1,41 @@
-console.log("Node is the 💣");
+console.log("Node is the 💣\n");
 
-const { createServer } = require("http");
+const http = require("http");
+const routes = require("./routes");
+const port = 8080;
 
-class ResponseBodyError extends Error {}
+function requestHandler(req, res) {
+  let { url, method } = req;
 
-let server = createServer((req, res) => {
-  const { url, method } = req;
-
-  let data;
-  let chunks = [];
-
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/html");
-
-  req.on("error", (err) => {
-    console.error(err);
-    res.statusCode = 400;
-    res.write("<h1>Bad Request >:(</h1>");
-    res.end();
-  });
-
-  res.on("error", (err) => {
-    console.error(err);
-    res.statusCode = 500;
-
-    if (err instanceof ResponseBodyError) {
-      res.write(`<h1>${err.message}</h1>`);
-    } else {
-      res.write("<h1>Server error, try again later...</h1>");
-    }
-
-    res.end();
-  });
+  const chunks = [];
 
   req.on("data", (chunk) => {
     chunks.push(chunk);
   });
 
   req.on("end", () => {
+    let reqBody = Buffer.concat(chunks).toString();
+
     try {
-      const reqBody = Buffer.concat(chunks).toString();
-
-      switch (url + method) {
-        case "/GET":
-          data = "<h1>Home</h1><a href='/newsletter'>Newsletter</a>";
-          break;
-        case "/newsletterGET":
-          data = "<h1>Newsletter</h1><a href='/'>Home</a>";
-          break;
-        case "/echoPOST":
-          res.setHeader("Content-Type", "application/json");
-          try {
-            data = JSON.stringify({
-              msg: "OK from the server",
-              ...JSON.parse(reqBody),
-            });
-          } catch (error) {
-            res.emit("error", error);
-          }
-          break;
-        default:
-          res.statusCode = 404;
-          data =
-            "<h1>Turn Back! This page doesn't exist.</h1><a href='/'>This may be what you're looking for...</a>";
+      reqBody = JSON.parse(reqBody);
+    } catch (err) {
+      let reqBodyObj = {};
+      let searchParams = new URLSearchParams(reqBody);
+      for (let prop of searchParams.keys()) {
+        reqBodyObj[prop] = searchParams.get(prop);
       }
-
-      res.write(data);
-      res.end();
-    } catch (error) {
-      res.emit(
-        "error",
-        new ResponseBodyError("Failed to send requested data.")
-      );
+      reqBody = reqBodyObj;
     }
 
-    console.log(
-      `${url}\t${method}\t${res.statusCode}\t${new Date().toLocaleTimeString()}`
-    );
-  });
-});
+    let route = routes[url + method] || routes["*"];
 
-server.listen(8080, () => {
-  console.log("Server listening on port 8080...");
-});
+    res.statusCode = route.statusCode;
+    res.setHeader("content-type", route.type);
+    res.write(route.body(reqBody));
+    res.end();
+  });
+}
+
+http
+  .createServer(requestHandler)
+  .listen(port, () => console.log("Server listening on port: " + port));
